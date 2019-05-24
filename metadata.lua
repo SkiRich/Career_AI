288db123fb03ce68141a9761906bf0e849fe7fac
SkiRich <richard@szkodzinski.com>
2019-05-23 20:57:27 -0400

    Upload to Steam

--- metadata.lua
+++ metadata.lua
@@ -1,8 +1,8 @@
 return PlaceObj('ModDef', {
 	'title', "Career A.I.",
-	'description', "Career A.I.\nv1.2.1 May 21st, 2019\nby SkiRich",
+	'description', "Career A.I.\nv1.3.0 May 23rd, 2019\nby SkiRich",
 	'image', "CareerAILogo.png",
-	'last_changes', "v1.2.1 May 21st, 2019",
+	'last_changes', "v1.3.0 May 23rd, 2019",
 	'ignore_files', {
 		"*.git/*",
 		"*.svn/*",
@@ -14,14 +14,14 @@ return PlaceObj('ModDef', {
 	'pops_desktop_uuid', "5078b6ce-3d7b-4ffd-9a3f-66c8b4b63d4c",
 	'pops_any_uuid', "3d7603a5-7498-4bfb-b964-e12bfca34e3a",
 	'author', "SkiRich",
-	'version', 16,
+	'version', 19,
 	'lua_revision', 233360,
 	'saved_with_revision', 244677,
 	'code', {
 		"Code/CAI_1Init.lua",
 		"Code/CAI_2ModConfig.lua",
 	},
-	'saved', 1558423677,
+	'saved', 1558659323,
 	'screenshot1', "Images/CareerAILogo.png",
 	'screenshot2', "Images/CAINotice.png",
 	'screenshot3', "Images/CAIModConfig.png",
